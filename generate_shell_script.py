f889e6347bfb1b16313926fa6e2e23bae9addc87
srikarym <msy290@nyu.edu>
2020-04-15 17:40:04 -0400

    .

--- generate_shell_script.py
+++ generate_shell_script.py
@@ -1,6 +1,6 @@
 import os
 
-exp_name = 'gridworld_new'
+exp_name = 'gridworld'
 
 write_to_file = ''
 
@@ -12,27 +12,43 @@ def create_command(flags):
     args = ["--%s %s" % (flag, str(flags[flag])) for flag in sorted(flags.keys())]
     jobcommand += " ".join(args)
 
-    write_to_file += jobcommand + ' & \n'
+    write_to_file += jobcommand + ' \n'
 
 
 args = {'num-steps': 128, 'num-mini-batch': 4, 'lr': 2.5e-4, 'algo': 'ppo',
-        'num-processes': 12, 'log-interval': 1, 'use-linear-lr-decay': '',
-        'use-pnn': '', 'num-env-steps': int(1E6)}
+        'num-processes': 20, 'log-interval': 1, 'use-linear-lr-decay': '',
+        'use-pnn': '', 'num-env-steps': int(5E6)}
 
-grid_size = 8
+grid_size = 16
 filename='ppo_run.sh'
 
 for seed in [0, 1, 2]:
     j = {k: v for k, v in args.items()}
-    j['n-columns'] = 2
     j['env-name'] = f'MiniGrid-Empty-{grid_size}x{grid_size}-v0'
     j['seed'] = seed
-    j['log-dir'] = f'./logs/{exp_name}/{grid_size}/pnn_5_{seed}'
-    j['save-dir'] = f'./trained_models/{exp_name}/{grid_size}/pnn_5_{seed}'
-    j['exp-name'] = f'{grid_size}_5_seed{seed}'
-    j['pnn-paths'] = f'trained_models/gridworld/5/ppo/MiniGrid-Empty-5x5-v0.pt'
+    j['log-dir'] = f'./logs/{exp_name}/{grid_size}/{seed}'
+    j['save-dir'] = f'./trained_models/{exp_name}/{grid_size}/{seed}'
+    j['exp-name'] = f'{grid_size}_seed{seed}'
+
 
     create_command(j)
+
+
+# for sg in [5]:
+#     for seed in [0, 1, 2]:
+#         j = {k: v for k, v in args.items()}
+#         j['n-columns'] = 2
+#         j['env-name'] = f'MiniGrid-Empty-{grid_size}x{grid_size}-v0'
+#         j['seed'] = seed
+#         j['log-dir'] = f'./logs/{exp_name}/{grid_size}/pnn_{sg}_{seed}'
+#         j['save-dir'] = f'./trained_models/{exp_name}/{grid_size}/pnn_{sg}_{seed}'
+#         j['exp-name'] = f'{grid_size}_{sg}_seed{seed}'
+#         if sg == 8:
+#             j['pnn-paths'] = f'trained_models/gridworld_new/{sg}/{seed}/ppo/MiniGrid-Empty-{sg}x{sg}-v0.pt'
+#         else:
+#             j['pnn-paths'] = f'trained_models/gridworld_new/{sg}/1/ppo/MiniGrid-Empty-{sg}x{sg}-v0.pt'
+
+#         create_command(j)
 
 with open(filename, 'w') as f:
     f.write(write_to_file)
